ea99a1a403d344b88bfcbc675a10a55c6e5ef555
AgathePons <ponsagathe37@gmail.com>
2023-01-17 16:15:51 +0100

    update script sql + script inser data

--- scripts/insert-data.ps1
+++ scripts/insert-data.ps1
@@ -2,5 +2,5 @@ Get-Content data-poe.sql | docker compose -p dbpoe-mysql-app exec -T db mysql -u
 Get-Content data-trainee.sql | docker compose -p dbpoe-mysql-app exec -T db mysql -u poe -ppoe post_suivi_stagiaire
 Get-Content data-survey.sql | docker compose -p dbpoe-mysql-app exec -T db mysql -u poe -ppoe post_suivi_stagiaire
 Get-Content data-question.sql | docker compose -p dbpoe-mysql-app exec -T db mysql -u poe -ppoe post_suivi_stagiaire
-Get-Content data-survey_contains_question.sql | docker compose -p dbpoe-mysql-app exec -T db mysql -u poe -ppoe post_suivi_stagiaire
+#Get-Content data-survey_contains_question.sql | docker compose -p dbpoe-mysql-app exec -T db mysql -u poe -ppoe post_suivi_stagiaire
 Get-Content data-answer.sql | docker compose -p dbpoe-mysql-app exec -T db mysql -u poe -ppoe post_suivi_stagiaire

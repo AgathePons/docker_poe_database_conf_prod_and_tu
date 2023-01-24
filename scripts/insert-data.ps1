8a846abfe78a7a64a4e7b369b740765bab5c6ccc
AgathePons <ponsagathe37@gmail.com>
2023-01-24 16:28:37 +0100

    update with new column insert + better trainees

--- scripts/insert-data.ps1
+++ scripts/insert-data.ps1
@@ -1,6 +1,6 @@
-Get-Content data-poe.sql | docker compose -p dbpoe-mysql-app exec -T db mysql -u poe -ppoe post_suivi_stagiaire
+#Get-Content data-poe.sql | docker compose -p dbpoe-mysql-app exec -T db mysql -u poe -ppoe post_suivi_stagiaire
+Get-Content data-poe2.sql | docker compose -p dbpoe-mysql-app exec -T db mysql -u poe -ppoe post_suivi_stagiaire
 Get-Content data-trainee.sql | docker compose -p dbpoe-mysql-app exec -T db mysql -u poe -ppoe post_suivi_stagiaire
 Get-Content data-survey.sql | docker compose -p dbpoe-mysql-app exec -T db mysql -u poe -ppoe post_suivi_stagiaire
 Get-Content data-question.sql | docker compose -p dbpoe-mysql-app exec -T db mysql -u poe -ppoe post_suivi_stagiaire
-#Get-Content data-survey_contains_question.sql | docker compose -p dbpoe-mysql-app exec -T db mysql -u poe -ppoe post_suivi_stagiaire
 Get-Content data-answer.sql | docker compose -p dbpoe-mysql-app exec -T db mysql -u poe -ppoe post_suivi_stagiaire

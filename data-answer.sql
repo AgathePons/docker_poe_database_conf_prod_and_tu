66a8f407445e81cd296b47fe4774a35a621860a3
nathan <nathan.viguier31@gmail.com>
2023-01-25 11:51:12 +0100

    mise a jour survey 6 mois deuxième retour

--- data-answer.sql
+++ data-answer.sql
@@ -11,10 +11,28 @@ insert into answer(id, text, question_id, order_in_question) values (9, '35k - 4
 insert into answer(id, text, question_id, order_in_question) values (10, '45k - 50k brut/an', 4, 4);
 insert into answer(id, text, question_id, order_in_question) values (11, '+ 50k brut/an', 4, 5);
 -- survey 2
-insert into answer(id, text, question_id, order_in_question) values (12, 'Vraiment pas bien', 8, 0);
-insert into answer(id, text, question_id, order_in_question) values (13, 'Encore debutant', 8, 1);
-insert into answer(id, text, question_id, order_in_question) values (14, 'De plus en plus confiant', 8, 2);
-insert into answer(id, text, question_id, order_in_question) values (15, 'Tres bien', 8, 3);
+
+
+insert into answer(id, text, question_id, order_in_question) values (12, 'Pas d''intégration à la fin de la période d''essai', 8, 0);
+insert into answer(id, text, question_id, order_in_question) values (13, 'Interuption de la période d''essai à votre initiative', 8, 1);
+insert into answer(id, text, question_id, order_in_question) values (14, 'Interruption de la période d''essai à l''initiative de l''entreprise', 8, 2);
+insert into answer(id, text, question_id, order_in_question) values (15, 'Changement d''entreprise', 8, 3);
+
+insert into answer(id, text, question_id, order_in_question) values (15, 'Angular', 8, 0);
+insert into answer(id, text, question_id, order_in_question) values (15, 'React', 8, 1);
+insert into answer(id, text, question_id, order_in_question) values (15, 'Vue', 8, 2);
+insert into answer(id, text, question_id, order_in_question) values (15, 'JS Natif', 8, 3);
+insert into answer(id, text, question_id, order_in_question) values (15, 'Autre framework frontend', 8, 4);
+insert into answer(id, text, question_id, order_in_question) values (15, 'Spring boot', 8, 5);
+insert into answer(id, text, question_id, order_in_question) values (15, 'JEE natif', 8, 6);
+insert into answer(id, text, question_id, order_in_question) values (15, 'Symfony', 8, 7);
+insert into answer(id, text, question_id, order_in_question) values (15, 'Laravel', 8, 8);
+insert into answer(id, text, question_id, order_in_question) values (15, 'PHP natif', 8, 9);
+insert into answer(id, text, question_id, order_in_question) values (15, 'Autre framework PHP', 8, 10);
+insert into answer(id, text, question_id, order_in_question) values (15, 'Python', 8, 11);
+insert into answer(id, text, question_id, order_in_question) values (15, '.Net', 8, 12);
+insert into answer(id, text, question_id, order_in_question) values (15, 'Autre tecnologies', 8, 13);
+
 -- survey 3
 insert into answer(id, text, question_id, order_in_question) values (16, 'Spring Boot', 12, 0);
 insert into answer(id, text, question_id, order_in_question) values (17, 'Angular', 12, 1);
--- data-question.sql
+++ data-question.sql
@@ -18,26 +18,36 @@ values (5, 'Etes-vous toujours dans votre entreprise ?', 'YES_NO', 4, 1);
 -- survey 2
 insert into question 
 (id, text, answer_type, order_in_survey, survey_id) 
-values (6, 'Comment vous appelez-vous ?', 'FREE', 0, 2);
+values (6, 'Etes-vous toujours en poste au sein de l''entreprise qui vous a initialement recruté ?', 'Yes_NO', 0, 2);
 insert into question 
 (id, text, answer_type, order_in_survey, survey_id) 
-values (7, 'Etes-vous toujours dans votre entreprise ?', 'YES_NO', 1, 2);
+values (7, 'Sinon, à quelle date êtes vous parti ?', 'FREE', 1, 2);
 insert into question 
 (id, text, answer_type, order_in_survey, survey_id) 
-values (8, 'Comment vous sentez-vous dans votre travail ?', 'CHOOSE_ONE', 2, 2);
+values (8, 'Si vous êtes parti, quelle en est la raison ?', 'CHOOSE_ONE', 2, 2);
 insert into question 
 (id, text, answer_type, order_in_survey, survey_id) 
-values (9, 'Avez-vous une remarque a faire pour nous aider a vous accompagner dans l''apres formation ?', 'FREE', 3, 2);
+values (9, 'Combien de mission différentes avez-vous effectué depuis votre intégration ?', 'FREE', 3, 2);
+insert into question 
+(id, text, answer_type, order_in_survey, survey_id) 
+values (10, 'Quelles technologies avez-vous utilisé au cours de vos différentes missions ?', 'CHOOSE_MANY', 4, 2);
+insert into question 
+(id, text, answer_type, order_in_survey, survey_id) 
+values (11, 'Avez-vous eu recours à des formations complémentaires ?', 'YES_NO', 5, 2);
+insert into question 
+(id, text, answer_type, order_in_survey, survey_id) 
+values (12, 'Sur une échelle de 1 à 10 comment estimez-vous votre propre progression en tant que Fullstack durant ces 6 mois ?', 'FREE', 6, 2);
+
 -- survey 3
 insert into question 
 (id, text, answer_type, order_in_survey, survey_id) 
-values (10, 'Comment vous appelez-vous ?', 'FREE', 0, 3);
+values (13, 'Comment vous appelez-vous ?', 'FREE', 0, 3);
 insert into question 
 (id, text, answer_type, order_in_survey, survey_id) 
-values (11, 'Etes-vous toujours dans votre entreprise ?', 'YES_NO', 1, 3);
+values (14, 'Etes-vous toujours dans votre entreprise ?', 'YES_NO', 1, 3);
 insert into question 
 (id, text, answer_type, order_in_survey, survey_id) 
-values (12, 'Sur quelles technos travaillez-vous maintenant', 'CHOOSE_MANY', 2, 3);
+values (15, 'Sur quelles technos travaillez-vous maintenant', 'CHOOSE_MANY', 2, 3);
 insert into question 
 (id, text, answer_type, order_in_survey, survey_id) 
-values (13, 'Accepteriez-vous de nous mettre un avis sur Google ?', 'YES_NO', 3, 3);
+values (16, 'Accepteriez-vous de nous mettre un avis sur Google ?', 'YES_NO', 3, 3);

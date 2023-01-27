9627dea178db826fc6a5b9a8c1c680e37c1735d5
Agui <madjerx@hotmail.fr>
2023-01-27 11:06:42 +0100

    bdd for prez

--- data-survey.sql
+++ data-survey.sql
@@ -1,4 +1,4 @@
 
-insert into survey(id, title, level, poe_type) values (1, 'Premier retour POEI', 'ONE_MONTH', 'POEI');
-insert into survey(id, title, level, poe_type) values (2, 'Deuxieme retour POEI', 'SIX_MONTHS', 'POEI');
-insert into survey(id, title, level, poe_type) values (3, 'Dernier POEI', 'ONE_YEAR', 'POEI');
+insert into survey(id, title, level, poe_type) values (1, 'Questionnaire 1 mois', 'ONE_MONTH', 'POEI');
+insert into survey(id, title, level, poe_type) values (2, 'Questionnaire 6 mois', 'SIX_MONTHS', 'POEI');
+insert into survey(id, title, level, poe_type) values (3, 'Questionnaire 1 an', 'ONE_YEAR', 'POEI');
